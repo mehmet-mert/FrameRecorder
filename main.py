3648023577cab439a92d5dc2336392cfe371f79c
mehmet-mert <mhmtmrtltnts@gmail.com>
2021-05-27 14:19:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import platform
 while True:
     try:
         import tkinter
@@ -8,13 +9,14 @@ while True:
         import cv2 as cv
         import pyautogui
         import webbrowser
+        break
     except:
         os.system("pip install opencv-python")
         os.system("pip install numpy")
         os.system("pip install PyAutoGUI")
-        pass
-    break
-
+        if platform.system() == "Linux":
+            print("You need to install python3-tk and scrot modules!")
+print()
 status = ""
 
 #Find the time for name
@@ -33,9 +35,9 @@ canvas = Canvas(root, bg="#4392F1", height=400, width=800, bd=0, highlightthickn
 canvas.place(x=0, y=0)
 background_img = PhotoImage(file=f"assets/background.png")
 background = canvas.create_image(400.0, 200.0, image=background_img)
-header = canvas.create_text(400.0, 91.0, text="Frame Recorder", fill="#ECE8EF",font=("Roboto-Bold", int(30.0)))
-create_label = canvas.create_text(203.5, 174.5, text="create an",fill="#ECE8EF",font=("Roboto-Bold", int(16.0)))
-video_label = canvas.create_text(590.5,174.5,text="fps video",fill="#ECE8EF",font=("Roboto-Medium", int(16.0)))
+header = canvas.create_text(400.0, 91.0, text="Frame Recorder", fill="#ECE8EF", font=("Roboto-Bold", int(30.0)))
+create_label = canvas.create_text(203.5, 174.5, text="create an", fill="#ECE8EF", font=("Roboto-Bold", int(16.0)))
+video_label = canvas.create_text(590.5, 174.5, text="fps video", fill="#ECE8EF", font=("Roboto-Medium", int(16.0)))
 switch = tkinter.Scale(from_=100, to=200, orient=tkinter.HORIZONTAL, length=200)
 
 #Start button command
@@ -63,7 +65,6 @@ def status_playing(yeter):
     if status == "don":
         pause["state"] = "disabled"
         start["state"] = "normal"
-        # info.config(text="Paused. Continue Recording with Play.")
         canvas.itemconfig(info, text="Paused. Continue Recording with Play")
     elif status == "playing":
         pause["state"] = "normal"
@@ -72,11 +73,9 @@ def status_playing(yeter):
         canvas.itemconfig(info, text="Recording...")
     elif status == "end":
         canvas.itemconfig(info, text="Video Saved At Outputs Folder. Let's Create Another One!")
-        #info.config(text="")
         pause["state"] = "disabled"
         end["state"] = "disabled"
         start["state"] = "normal"
-        # start.config(text="+ New")
 
 start_img = PhotoImage(file=f"assets/start.png")
 start = Button(image=start_img, borderwidth=0, highlightthickness=0, command=lambda :start_record(), relief="flat")
@@ -90,16 +89,12 @@ info = canvas.create_text(400.0, 342.5, text="Start Recording", fill="#ECE8EF", 
 end["state"] = "disabled"
 pause["state"] = "disabled"
 
-def callback(event):
-    webbrowser.open_new(event.widget.cget("text"))
-watermark = tkinter.Label(root, text=r"https://github.com/mehmet-mert", fg="blue", cursor="hand2")
 while True:
     root.update()
     switch.place(x=400, y=176, anchor=tkinter.CENTER)
     start.place(x=318, y=230, width=172, height=58)
     pause.place(x=118, y=230, width=172, height=58)
     end.place(x=518, y=230, width=172, height=58)
-
     if status == "playing":
         record()
     elif status == "don":
